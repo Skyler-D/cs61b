84c9abefe1d4f0ff248a015429886b7b9d5b32f2
Skyler <skyler.d.y.s@gmail.com>
2025-05-03 13:10:47 +0800

    create Planet.java, 2 constructors, method calcDistance and method calcForceExertedBy

---
+++ proj0/Planet.java
@@ -0,0 +1,40 @@
+public class Planet{
+	public double xxPos = 0.0;
+	public double yyPos = 0.0;
+	public double xxVel = 0.0;
+	public double yyVel = 0.0;
+	public double mass = 0.0;
+	public String imgFileName = "jupiter.gif";
+	
+	static final double G = 6.67e-11;
+	
+	
+	public Planet(double xP, double yP, double xV, double yV, double m, String img){
+		xxPos = xP;
+		yyPos = yP;
+		xxVel = xV;
+		yyVel = yV;
+		mass = m;
+		imgFileName = img;
+	}
+	
+	public Planet(Planet p){
+		this.xxPos = p.xxPos;
+		this.yyPos = p.yyPos;
+		this.xxVel = p.xxVel;
+		this.yyVel = p.yyVel;
+		this.mass = p.mass;
+		this.imgFileName = p.imgFileName;
+	}
+	
+	public double calcDistance(Planet p){
+		double dx = p.xxPos - this.xxPos;
+		double dy = p.yyPos - this.yyPos;
+		return Math.sqrt(dx * dx + dy * dy);
+	}
+	
+	public double calcForceExertedBy(Planet p){
+		double r = this.calcDistance(p);
+		return (G * this.mass * p.mass / (r * r));
+	}
+}
