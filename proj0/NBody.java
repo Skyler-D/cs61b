3dbc9f4f798cbd02a88e2ed0e4a68b6d3b23ae88
Skyler <skyler.d.y.s@gmail.com>
2025-05-03 20:54:34 +0800

    modified method readRadius through deleting 'Int n = '

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -6,7 +6,7 @@
 public class NBody{
 	public static double readRadius(String fileName){
 		In in = new In(fileName);
-		int N = in.readInt();
+		in.readInt(); // 读取并丢弃 5
 		double R = in.readDouble();
 		return R;
 	}
